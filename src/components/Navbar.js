a79d7c2de9b5b79441b874c7f9bc7daf1cc8c675
mwaniel <mwaniel03@gmail.com>
2023-03-31 20:06:17 +0300

    bootstrap import  and icons import plus font ,

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,20 @@ import React from 'react';
 const Navbar = () => {
   return (
     <div>
-      hellow
+      <nav className="navbar navbar-light bg-dark mb-5 ">
+        <div className="container">
+            <div className="navbar-header">
+                <a className="navbar-brand text-white text-lg brand-text" href="#">
+                Movies App
+                </a>
+            </div>
+            <ul className="navbar-nav ml-auto text-light d-inline-block">
+                <li className="nav-item d-inline-block mr-4">
+                    <i class="fab fa-youtube fa-5x" id="youtube-logo"></i>
+                </li>
+            </ul>
+        </div>
+      </nav>
     </div>
   );
 }
---
+++ src/components/Text1.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Text1 = () => {
+  return (
+    <div>Text1</div>
+  )
+}
+
+export default Text1
